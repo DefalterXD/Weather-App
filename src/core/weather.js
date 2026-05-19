dd3e5a5a407f363420a3248e6698a3a009f100a4
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-05-20 00:56:35 +0500

    feat: add async function for fetching API

---
+++ src/core/weather.js
@@ -0,0 +1,13 @@
+export const getLocation = async function getLocationFromAPI(location) {
+    try {
+        const dataLocation = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=W3KQHZNRSNUWH75XWU46P53YY`);
+        if(!dataLocation.ok) {
+            console.error('The request was not found');
+        } 
+        const json = await dataLocation.json();
+        jsonDataProcessing(json);
+    } catch(error) {
+        console.log(error);
+    }
+};
+
