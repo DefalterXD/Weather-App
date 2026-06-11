7c495aaa4a7bd3cfa16d2665225c825495b2a4e6
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-06-11 19:48:44 +0500

    fix: add condition for adding city reference and city result

--- src/interface/searchCity.js
+++ src/interface/searchCity.js
@@ -1,19 +1,28 @@
-import { getLocation } from "../core/weather.js";
+import { getLocation, tempMode } from "../core/weather.js";
 import { displayData } from "./displayData.js";
 
 const searchField = document.querySelector('#city');
 const searchButton = document.querySelector('.searchCity');
 
 const checkInput = async function checkInputForValidValue(e) {
-    e.preventDefault();
-    if (!searchField.value) {
-        console.log('No value was added');
-        return;
+    
+    try {
+        e.preventDefault();
+        if (!searchField.value) {
+            console.log('No value was added');
+            return;
+        }
+        const city = searchField.value;
+        const cityResult = await getLocation(city);
+        
+        // IF the result exist then proceed 
+        if (cityResult) {
+            tempMode.cityReference = cityResult;
+            displayData(cityResult);
+        }
+    } catch (error) {
+        console.log(error);
     }
-
-    const city = searchField.value;
-    const cityResult = await getLocation(city);
-    displayData(cityResult);
-}
+};
 
 searchButton.addEventListener('click', checkInput);
