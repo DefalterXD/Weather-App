f7555a830a2475ae39397d60a2098ceba15334ce
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-06-11 19:30:02 +0500

    feat: render a 'meta' section into DOM

---
+++ src/interface/displayMeta.js
@@ -0,0 +1,25 @@
+export const displayMetaData = function displayMetaDataOnThePage(resolvedAddress, tempDescription, cityTemp) {
+    const metaSection = document.createElement('section');
+    metaSection.classList.add('meta__container');
+
+    const addressTitle = document.createElement('h1');
+    addressTitle.classList.add('address__title');
+    // SPLIT the address to get only the city name
+    addressTitle.textContent = resolvedAddress.split(',')[0].toString();
+
+
+    const currentDesc = document.createElement('h2');
+    currentDesc.classList.add('current__desc');
+    currentDesc.textContent = tempDescription;
+
+    const currentTemp = document.createElement('h1');
+    currentTemp.classList.add('current__temp');
+    currentTemp.textContent = cityTemp;
+
+    const tempModeButton = document.createElement('button');
+    tempModeButton.classList.add('button__temp');
+
+    metaSection.append(addressTitle, currentDesc, currentTemp, tempModeButton);
+
+    return metaSection;
+};
