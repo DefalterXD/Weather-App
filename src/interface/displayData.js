ad339cb8e46a170b7218a4e2104c03e38ab6fe5b
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-06-11 19:59:06 +0500

    fix: delete duplicate DOM sections

--- src/interface/displayData.js
+++ src/interface/displayData.js
@@ -6,8 +6,28 @@ import { displayWeekData } from "./displayWeek.js";
 
 const mainWeatherContainer = document.querySelector('.weather__container');
 
+const deleteDuplicateSections = function deleteDuplicateSectionsAfterCityQuery() {
+    const metaSection = document.querySelector('.meta__container');
+    const conditionSection = document.querySelector('.condition__container');
+    const hourSection = document.querySelector('.hour__container');
+    const weekSection = document.querySelector('.week__container');
+
+    if (metaSection) {
+        metaSection.remove();
+    } 
+    if (conditionSection) {
+        conditionSection.remove();
+    }
+    if (hourSection) {
+        hourSection.remove();
+    } 
+    if (weekSection) {
+        weekSection.remove();
+    }
+}
 
 export const displayData = function displayDataAboutCityInPage(city) {
+    deleteDuplicateSections();
     const metaDataElement = displayMetaData(city.resolvedAddress, city.conditions, city.temp);
     const conditionDataElement = displayConditionData(city.feelslike, city.windspeed, city.uvindex, city.firstWeekDays[0].dew);
     const hourDataElement = displayHoursData(city.firstWeekDays[0].hours);
