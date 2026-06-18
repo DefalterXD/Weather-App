415b7c428b37c4d90e67e9c59ee4a5b624a6979a
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-06-18 09:44:42 +0500

    feat: add 'populate' function for changed temp mode

--- src/interface/tempMode.js
+++ src/interface/tempMode.js
@@ -1,3 +1,4 @@
+import { populateLocalStorage } from "../core/localStorage.js";
 import { tempMode } from "../core/weather.js";
 import { displayData } from "./displayData.js";
 
@@ -8,10 +9,12 @@ export const tempModeButtonSwitch = (buttonElement) => {
             tempMode.isCelsius = true;
             buttonElement.setAttribute('temp-mode', 'C°');
             displayData(tempMode.cityCelsius);
+            populateLocalStorage();
         } else {
             tempMode.isCelsius = false;
             buttonElement.setAttribute('temp-mode', 'F°');
             displayData(tempMode.cityFahrenheit);
+            populateLocalStorage();
         }
     });
 }
