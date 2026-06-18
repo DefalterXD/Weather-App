5f347168746b37ba4b010270f835e1bb0c60f624
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-06-18 10:25:39 +0500

    fix: apply prettier

--- src/core/localStorage.js
+++ src/core/localStorage.js
@@ -26,7 +26,8 @@ export const storageAvailable = function storageAvailableInBrowser(type) {
 
 export const populateLocalStorage = function populateLocalStorageFromWeather() {
   localStorage.setItem("isCelsius", JSON.stringify(tempMode.isCelsius));
-  if (tempMode.cityName !== '') localStorage.setItem("cityName", JSON.stringify(tempMode.cityName));
+  if (tempMode.cityName !== "")
+    localStorage.setItem("cityName", JSON.stringify(tempMode.cityName));
 };
 
 export const renderPageFromLocalStorage =
