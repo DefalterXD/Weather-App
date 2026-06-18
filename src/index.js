8ba2686f26592565e284955af549a72bf766ce89
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-06-18 09:45:44 +0500

    feat: use localStorage after page reload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './reset.css';
 import './style.css';
 import './interface/searchCity.js';
+import { renderPageFromLocalStorage, storageAvailable } from './core/localStorage.js';
+import { hideLoading, showLoading } from './interface/loadingComponent.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    showLoading();
+    if (storageAvailable('localStorage')) {
+        if (localStorage.getItem('isCelsius') && localStorage.getItem('cityName')) {
+            renderPageFromLocalStorage();
+        } else {
+            hideLoading();
+        }
+    } else {
+        console.log("Sorry your browser doesn't support localStorage");
+    }
+
+});
