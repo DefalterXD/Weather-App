ddc1bfb2616772b390ad8130176b48f8ad361107
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-06-18 09:31:55 +0500

    feat: add 'loading' component for fetching process

---
+++ src/interface/loadingComponent.js
@@ -0,0 +1,9 @@
+const loadingComponent = document.querySelector('.loader__container');
+
+export const showLoading = function showLoadingOnPage() {
+    loadingComponent.classList.remove('hidden');
+};
+
+export const hideLoading = function hideLoadingOnPage() {
+    loadingComponent.classList.add('hidden');
+};
